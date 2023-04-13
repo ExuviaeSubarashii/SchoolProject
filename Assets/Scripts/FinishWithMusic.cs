962d6effb6ed0296801fc0fd4974fca3107cc08c
Ali Küçükkaya <preoyuncu@hotmail.com>
2023-04-13 12:43:47 +0300

    Timer Fixed | Music Sound Decreased
    Timer Fixed | Music Sound Decreased

--- Assets/Scripts/FinishWithMusic.cs
+++ Assets/Scripts/FinishWithMusic.cs
@@ -5,18 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class FinishWithMusic : MonoBehaviour
 {
-    public float duration=0.0f;
+    public float duration = 0.0f;
+    public int seconds = 0;
+    public int minutes;
     void Update()
     {
         duration += Time.deltaTime;
-        int seconds = (int)(duration % 60);
-        if (SceneManager.GetActiveScene().buildIndex == 2 && seconds == 30)
+
+        minutes = Mathf.FloorToInt(duration / 60F);
+        seconds = Mathf.FloorToInt(duration - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        if (SceneManager.GetActiveScene().buildIndex == 2 && minutes == 2 && seconds == 55)
         {
             ScoreManager.instance.CompleteLevel();
+            Debug.Log("Level 1 Complete");
         }
-        if (SceneManager.GetActiveScene().buildIndex == 3 && seconds == 30)
+        if (SceneManager.GetActiveScene().buildIndex == 3 && minutes == 4 && seconds == 04)
         {
             ScoreManager.instance.CompleteLevel();
+            Debug.Log("Level 2 Complete");
         }
     }
 }
